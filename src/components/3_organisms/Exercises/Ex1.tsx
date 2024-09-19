72ccabc2c2c4cabaa3fdbf1f284a8e196103888c
GooseUchonyj <goose.uchonyj@gmail.com>
2024-09-19 19:00:25 +0300

    добавляю svg изображения1

--- src/components/3_organisms/Exercises/Ex1.tsx
+++ src/components/3_organisms/Exercises/Ex1.tsx
@@ -8,9 +8,23 @@ function Ex1() {
       <h3 id={"Ex1"}>Exercise1</h3>
       <div className={styles.avito_mainblock}>
         <div className={"avito_title"}><h2 className={styles.avito_title}>Светильник Дракон</h2></div>
-        <div className={"avito_img"}><img src="/img/draco1.jpg"
-                                          width={"100%"} alt="Picture background" aria-hidden="true"/></div>
-        <div className={"avito_price"}><h2>2 760 ₽</h2>
+        <div className={"avito_gallery"}>
+
+          </div>
+
+          <div className={"avito_img"}>
+            <div className={"frame_control"} id={"left"}>
+              <button></button>
+            <img src="/img/draco1.jpg" width={"100%"}
+                 alt="Picture background" aria-hidden="true"/>
+            <div className={"frame_control"} id={"right"}>
+              <button></button>
+            </div>
+          </div>
+
+        </div>
+
+        <div className={"avito_price"}><h2 className={styles.avito_price}>2 760 ₽</h2>
           <div className={"like"}></div>
         </div>
         <div className={styles.avito_buttons}>
@@ -19,6 +33,14 @@ function Ex1() {
         </div>
 
       </div>
+
+      1. Название свойства transition-property
+      2. Продолжительность перехода transition-duration
+      3. Функция перехода transition-timing-function
+      4. Задержка перехода transition-delay
+      5. Краткая запись перехода transition
+      6. Плавный переход нескольких свойств
+      7. Примеры переходов для различных свойств
 
     </section>)
 }
