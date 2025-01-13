eb2f90d4030676433be0ad74bfde1ecd7fbc1be1
GooseUchonyj <goose.uchonyj@gmail.com>
2025-01-13 20:57:24 +0300

    [хочу переместить шрифт в другую папку, а для этого нужно закрыть программу. нет изменений

--- src/components/3_organisms/Exercises/Ex1.tsx
+++ src/components/3_organisms/Exercises/Ex1.tsx
@@ -10,9 +10,7 @@ function Ex1() {
         <div className={styles.avito_mainblock}>
           <div className={"avito_title"}><h2 className={styles.avito_title}>Светильник Дракон</h2></div>
           <div className={"avito_gallery"}>
-
           </div>
-
           <div className={styles.avito_img}>
             <div className={"frame_control"} id={"left"}>
               <button></button>
@@ -22,9 +20,7 @@ function Ex1() {
             <div className={"frame_control"} id={"right"}>
               <button></button>
             </div>
-
           </div>
-
           <div className={"avito_price"}><h2 className={styles.avito_price}>2 760 ₽</h2>
             <div className={"like"}></div>
           </div>
@@ -43,7 +39,7 @@ function Ex1() {
         7. Примеры переходов для различных свойств */}
 
         <div className={"mts_form"}>
-          <div><h1 className={styles.mts_title}>ЛИЧНЫЙ КАБИНЕТ</h1></div>
+          <div><h1 className={"mts_title"}>ЛИЧНЫЙ КАБИНЕТ</h1></div>
           <div><p>Максимум возможностей </p>
             <p>для управления счётом </p>
             <p>и услугами мобильной связи</p></div>
